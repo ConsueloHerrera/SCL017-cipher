d1a8655628ade43014a23f6d474b6c3d8ca81233
ConsueloHerrera <83927184+ConsueloHerrera@users.noreply.github.com>
2021-05-15 19:02:22 -0400

    git

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+document.getElementById("botonEntrar").onclick = function(){
+        document.getElementById("saludo").toggleAttribute("hidden")
+
+document.getElementById("entrar").toggleAttribute("hidden")
+}
+
