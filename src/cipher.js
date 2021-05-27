5f4ddad4b7d28a089a96a517410226656b85f0c6
ConsueloHerrera <consueloherrerabarguena@gmail.com>
2021-05-27 10:55:33 -0400

    realizacion de html pagina descifrado, funcionalidades de botones cifrar, funcion cifrado en js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,24 @@
+const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 const cipher = { 
+  encode: (texto, desplazamiento) => { 
+    let resultado = "";
+
+    desplazamiento = ((desplazamiento % 26) + 26) %26;
+
+    if (texto) {
+
+      for(let i = 0; i < texto.length; i++) {
+        if (letras.indexOf(texto[i]) != -1) {
+          let posicion = (letras.indexOf (texto[i]) + desplazamiento) % 26;
+          resultado += letras[posicion];
+          } else { 
+            resultado += texto [i];
+          } 
+      }
+    }
+return resultado;
   // ...
-};
+} }
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
+
+// funciones interfaz página
+
 document.getElementById("botonEntrar").onclick = function(){
         document.getElementById("saludo").toggleAttribute("hidden")
 
@@ -9,15 +12,21 @@ document.getElementById("entrar").toggleAttribute("hidden")
 }
 
 document.getElementById("botonCifrar").onclick = function(){
-        document.getElementById("cifrar").toggleAttribute("hidden")
+        document.getElementById("cifrarMensaje").toggleAttribute("hidden")
 
 document.getElementById("saludo").toggleAttribute("hidden")
 }
 
-document.getElementById("botonEncode").onclick = function(){
+document.getElementById("botonDescifrar").onclick = function(){
+        document.getElementById("textoDescifrado").toggleAttribute("hidden")
+
+document.getElementById("saludo").toggleAttribute("hidden")
+}
+
+document.getElementById("cifrar").onclick = function(){
         document.getElementById("textoCifrado").toggleAttribute("hidden")
 
-document.getElementById("cifrar").toggleAttribute("hidden")
+document.getElementById("cifrarMensaje").toggleAttribute("hidden")
 
 }
 document.getElementById("botonPrincipal").onclick = function(){
@@ -25,5 +34,27 @@ document.getElementById("botonPrincipal").onclick = function(){
 
         document.getElementById("textoCifrado").toggleAttribute("hidden")
 }
+document.getElementById("boton2").onclick = function(){
+        document.getElementById("textoDescifrado").toggleAttribute("hidden")
 
+document.getElementById("textoCifrado").toggleAttribute("hidden")
+}
+document.getElementById("botonPrincipal2").onclick = function(){
+        document.getElementById("saludo").toggleAttribute("hidden")
 
+        document.getElementById("textoDescifrado").toggleAttribute("hidden")
+}
+
+// cifrado
+const cifrar = document.getElementById("cifrar"); 
+
+cifrar.addEventListener ("click", function () {
+        let mensaje = document.getElementById("mensaje").value;
+        let mensajeMayuscula = mensaje.toUpperCase();
+        let desplazamiento = document.getElementById("desplazamiento").value;
+
+        let mensajeCifrado = cipher.encode (mensajeMayuscula, desplazamiento);
+
+        document.getElementById("mensaje2").value=mensajeCifrado;
+       
+})
